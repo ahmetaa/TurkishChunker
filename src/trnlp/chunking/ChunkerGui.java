cace4e29d3dd693c6d2dd488c563357cc35bd351
ahmetaa <ahmetaa@gmail.com>
2013-05-14 22:29:21 +0300

    test with no-punctuation

--- src/trnlp/chunking/ChunkerGui.java
+++ src/trnlp/chunking/ChunkerGui.java
@@ -16,13 +16,8 @@ public class ChunkerGui {
      */
     public static void main(String args[]) throws IOException {
 
-        final TurkishChunker chunker = new TurkishChunker(new File("src/tr/models/chunk-model2.ser"));
+        final TurkishChunker chunker = new TurkishChunker(new File("src/tr/models/chunk-model-nopunct.ser"));
 
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-         */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
@@ -30,22 +25,15 @@ public class ChunkerGui {
                     break;
                 }
             }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (Exception e) {
+            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, e);
         }
-        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    new NewJFrame(chunker).setVisible(true);
+                    new NewJFrame(chunker, true).setVisible(true);
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
@@ -147,7 +135,8 @@ class NewJFrame extends javax.swing.JFrame {
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {
         String input = jTextField1.getText();
-
+        if (eliminatePunctuations)
+            input = ChunkerFeatureExtractor.eliminatePunctuations(input);
 
         String tokenized = tokenizer.getTokensContentsAsString(input);
         List<String> tokenList = tokenizer.tokenizeAsStrings(input);
@@ -165,7 +154,6 @@ class NewJFrame extends javax.swing.JFrame {
     }
 
     String getTurkish(List<Chunk> chunks) {
-
         StringBuilder builder = new StringBuilder();
         int i = 0;
         for (Chunk chunk : chunks) {
